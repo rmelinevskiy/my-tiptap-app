8cd4359f036e53c31079a45031a6c660f352bebc
rmelinevskiy <150797126+Arkadaweb@users.noreply.github.com>
2024-09-13 22:53:02 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,195 @@
+// src/App.jsx
+import {EditorProvider, FloatingMenu, BubbleMenu, useEditor, EditorContent,} from '@tiptap/react'
+import {Document} from '@tiptap/extension-document';
+import {Text} from '@tiptap/extension-text';
+import {HocuspocusProvider} from '@hocuspocus/provider';
+import {useEffect, useMemo, useRef, useState} from "react";
+import * as Y from 'yjs'
+import StarterKit from '@tiptap/starter-kit'
+import Collaboration from '@tiptap/extension-collaboration'
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
+
+const ydoc = new Y.Doc();
+
+const extensions = [StarterKit]
+
+const App = () => {
+
+  const refhoc = useRef<any>(null)
+
+  const [data, setData] = useState<any>(null);
+
+  const provider = useMemo(
+    () =>
+      new HocuspocusProvider({
+        url: "ws://127.0.0.1:1234/collaboration",
+        name: "example-document",
+        document: ydoc,
+      }),
+    []
+  );
+
+  console.log('provider')
+  console.log(provider)
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Collaboration.configure({
+        document: ydoc,
+      }),
+      CollaborationCursor.configure({
+        provider,
+        user: { name: "John Doe", color: "#ffcc00" },
+      }),
+    ],
+    content: ``,
+    onUpdate: ({editor}) => provider.on('outgoingMessage', () => {editor.getJSON()}),
+    // onTransaction: ({editor }) => setData(editor),
+  })
+
+  // console.log('refhoc')
+  // console.log(data.getJSON())
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          maxWidth: 1200,
+          margin: '0 auto'
+        }}
+      >
+        <MenuBar editor={editor} />
+        <EditorContent editor={editor}
+                       slotBefore={<div>111</div>}
+                       slotAfter={<div>222</div>}
+        />
+        <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+        <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+      </div>
+    </div>
+  );
+};
+
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className="control-group">
+      <div className="button-group">
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          Bold
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          Italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          Strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          className={editor.isActive('code') ? 'is-active' : ''}
+        >
+          Code
+        </button>
+        <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+          Clear marks
+        </button>
+        <button onClick={() => editor.chain().focus().clearNodes().run()}>
+          Clear nodes
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive('paragraph') ? 'is-active' : ''}
+        >
+          Paragraph
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        >
+          H1
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        >
+          H2
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+        >
+          H3
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+          className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+        >
+          H4
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+          className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+        >
+          H5
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+          className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+        >
+          H6
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          Bullet list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          Ordered list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={editor.isActive('codeBlock') ? 'is-active' : ''}
+        >
+          Code block
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive('blockquote') ? 'is-active' : ''}
+        >
+          Blockquote
+        </button>
+        <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+          Horizontal rule
+        </button>
+        <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+          Hard break
+        </button>
+        <button onClick={() => editor.chain().focus().undo().run()}>
+          Undo
+        </button>
+        <button onClick={() => editor.chain().focus().redo().run()}>
+          Redo
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
